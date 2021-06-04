4047ee03a7a5110ad4e1298dd1d161e6199c4945
soneshaps <sonesh.aps@gmail.com>
2021-06-04 13:55:16 +0545

    route deleted

--- assignment/node-ex-3/index.js
+++ assignment/node-ex-3/index.js
@@ -1,40 +1,8 @@
 const express = require("express");
 const app = express();
-// const fs = require("fs");
-// const path = require("path");
-// const dbPath = path.join(__dirname, "file.json");
 
 app.use(express.json());
-const router = express.Router();
 const signupRouter = require("./router/signup.router");
-
-// const bycrypt = require("bcryptjs");
-// // //controller
-// // function registerController(res, req, next) {
-// //   const { username, password } = req.body;
-
-// // }
-
-// //routes
-// app.use(
-//   "/api",
-//   router.route("/register").post((req, res, next) => {
-//     const { username, password } = req.body;
-//     const hashpassword = bcrypt.genSalt(10, function (err, salt) {
-//       bcrypt.hash(password, salt, function (err, hash) {
-//         return hash;
-//       });
-//     });
-
-//     let saveUser = { username, password };
-//     fs.writeFile(dbPath, saveUser, (err) => {
-//       if (err) {
-//         console.log(err);
-//         return;
-//       }
-//     });
-//   })
-// );
 
 app.use("/api", signupRouter);
 
